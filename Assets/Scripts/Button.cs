8c55d029428e02789a574ce9c47082de4b0986ae
Hietala Veeti TTK18SP <veetihietala@kamk.fi>
2019-01-26 00:16:51 +0200

    Button scriptin alkua

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public Sprite deactivatedButton;
+    public Sprite activatedButton;
+    public float requiredMass;
+
+    public GameObject door;
+
+    private float triggerMass;
+    private Rigidbody2D triggerRb;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        triggerRb = other.gameObject.GetComponent<Rigidbody2D>();
+
+        triggerMass = triggerRb.mass;
+
+        if (triggerMass >= requiredMass)
+        {
+            gameObject.transform.position = new Vector2(transform.position.x, transform.position.y - 0.17f);
+            OnPressed();
+        }
+    }
+
+    public void OnPressed()
+    {
+        Debug.Log("Button Pressed");
+    }
+}
+
