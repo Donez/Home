fa6c206256502fc1c861796817f99d875e5f03c6
Hietala Veeti TTK18SP <veetihietala@kamk.fi>
2019-01-26 11:24:02 +0200

    Button scripti ja placeholderit

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Button : MonoBehaviour
 {
@@ -8,33 +9,65 @@ public class Button : MonoBehaviour
     public Sprite activatedButton;
     public float requiredMass;
 
+    public Sprite closedDoor;
+    public Sprite openedDoor;
+
     public GameObject door;
 
     private float triggerMass;
     private Rigidbody2D triggerRb;
 
+    public bool holdButton = false;
+
+    private GameObject triggerObj;
+
     // Update is called once per frame
     void Update()
     {
-
+        Debug.Log(triggerObj);
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        
         triggerRb = other.gameObject.GetComponent<Rigidbody2D>();
 
         triggerMass = triggerRb.mass;
 
         if (triggerMass >= requiredMass)
         {
-            gameObject.transform.position = new Vector2(transform.position.x, transform.position.y - 0.17f);
-            OnPressed();
+            triggerObj = other.gameObject;
+            gameObject.GetComponent<SpriteRenderer>().sprite = activatedButton;
+            OpenDoor();
         }
     }
 
-    public void OnPressed()
+    private void OnTriggerExit2D(Collider2D col)
     {
-        Debug.Log("Button Pressed");
+        if (col.gameObject == triggerObj)
+        {
+            if (holdButton == true)
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = deactivatedButton;
+
+                CloseDoor();
+            }
+        }
+
+
     }
+
+    public void OpenDoor()
+    {
+        gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = openedDoor;
+        gameObject.transform.GetChild(0).GetComponent<Collider2D>().enabled = false;
+    }
+
+    public void CloseDoor()
+    {
+        gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = closedDoor;
+    }
+   
+
 }
 
