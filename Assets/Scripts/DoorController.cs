92a3bae8802c3951843ab87f828f77cafba61340
Rantaniemi Matti TTK18SP <mattirantaniemi@kamk.fi>
2019-01-26 22:00:41 +0200

    New hub

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorController : MonoBehaviour
+{
+
+    public Sprite sprite1;
+    public Sprite sprite2;
+    public SpriteRenderer spr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            spr.sprite = sprite2;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D colliosion)
+    {
+        if(colliosion.gameObject.name == "Player")
+        {
+            spr.sprite = sprite1;
+        }
+    }
+
+}
