f7e824231cf9606c2b09a30c518617e888b3233d
JONI\joniv <Joni@CriticalForce.fi>
2019-01-26 15:02:43 +0200

    Added cutscene

---
+++ Assets/Scripts/AnimationCutscene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [RequireComponent(typeof(Animation))]
+    public class AnimationCutscene : Cutscene
+    {
+        void Awake()
+        {
+            var animation = GetComponent<Animation>();
+
+            var lengthSeconds = animation.clip.length;
+
+            Destroy(gameObject, lengthSeconds);
+        }
+    }
+}
---
+++ Assets/Scripts/CutsceneTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class CutsceneTrigger : MonoBehaviour
+    {
+        public Cutscene CutscenePrefab;
+
+        void OnTriggerEnter2D(Collider2D col)
+        {
+            Game.PlayCutscene(CutscenePrefab);
+        }
+
+        void OnTriggerExit2D(Collider2D col)
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
